7882fb13be92fc88876745469492c491923d790c
saltchicken <john.eicher89@gmail.com>
2023-05-27 14:27:30 -0700

    Update main

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 import sys
 from tasker import Tasker
 
-if __name__ == '__main__':
-    print('Running Tasker')
-    app = Tasker(sys.argv)
-    sys.exit(app.exec_())
+
+print('Running Tasker')
+app = Tasker(sys.argv)
+sys.exit(app.exec_())
